1a97bed2fcac04087d9091c9bdf9d9183ac70575
ogheneruno <orunosaduwa@gmail.com>
2019-12-05 22:09:36 +0100

    test

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './Card.css';
 import prj from '../../images/akumlate.png';
 
-const Card = ({title, description, img}) => {
-    return (
-        <div className="card">
-            <h1>CGPA Calculator</h1>
-            <p>a cgpa Calculator to calculate students cgpa</p>
-            <div className="imgBox">
-            <img className="prjImage" src={prj} alt="project"/>
+const Card = ({ title, description, img }) => {
+	return (
+		<div className="card">
+			<h1>CGPA Calculator</h1>
+			<p>a cgpa Calculator to calculate students cgpa</p>
+			<div className="imgBox">
+				<img className="prjImage" src={prj} alt="project" />
+			</div>
+			<div className="btn-container">
+            <a className="project-btn live-link" href="#live">live</a>
+			<a className="project-btn code-link" href="#code">
+				<i className="fab fa-github" /> Code
+			</a>
             </div>
-            <button>View Case Study</button>
-        </div>
-    )
-}
-
-
+		</div>
+	);
+};
 
 export default Card;
