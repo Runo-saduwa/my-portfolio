4888d59a482de09ea28044c0aa12aef10a93a411
ogheneruno <orunosaduwa@gmail.com>
2019-12-09 14:22:54 +0100

    test

--- src/Context.js
+++ src/Context.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react';
 export const themeContext = createContext();
 
 export const Provider = (props) => {
-	const [ darkMode, setDarkModeState ] = useState(true);
+	const [ darkMode, setDarkModeState ] = useState(false);
 
 	const toggleDarkMode = () => {
 		setDarkModeState(!darkMode);
